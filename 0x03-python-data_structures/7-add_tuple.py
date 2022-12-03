f4d904d7cce72fe54f0db02dd325225551308779
phemsie <femi_oworu@yahoo.com>
2022-12-03 15:58:59 -0800

    python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-""" adds two tuples
-"""
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    """ this function adds 2 tuples"""
-    if len(tuple_a) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-        if len(tuple_b) > 2:
-            tuple_b = (tuple_b[0], tuple_b[1])
-            if len(tuple_a) == 1:
-                tuple_a = (tuple_a[0], 0)
-                if len(tuple_b) == 1:
-                    tuple_b = (tuple_b[0], 0)
-                    if len(tuple_a) == 0:
-                        tuple_a = (0, 0)
-                        if len(tuple_b) == 0:
-                            tuple_b = (0, 0)
-                            sum_tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-                            return sum_tup
+    tup1 = tuple_a + (0, 0)
+    tup2 = tuple_b + (0, 0)
+    result = (tup1[0] + tup2[0], tup1[1] + tup2[1])
+    return (result)
