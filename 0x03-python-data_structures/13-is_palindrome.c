0b58234ccd85e87a9cf88ae8f97405820738b898
phemsie <femi_oworu@yahoo.com>
2022-12-03 16:12:07 -0800

    python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,42 +1,42 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 
 /**
- * is_palindrome - check if linked list is a palindrome
- * @head: pointer to pointer to head of the list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * PalindromeRecur - Check if linked list is palindrome.
+ * @left: Head of list
+ * @right: Last element take it in recursion.
+ */
+
+int PalindromeRecur(listint_t **left, listint_t *right)
+{
+	int pal;
+
+	if (right == NULL)
+		return (1);
+
+	pal = PalindromeRecur(left, right->next);
+	if (pal == 0)
+		return (0);
+
+	pal = (right->n == (*left)->n);
+
+	*left = (*left)->next;
+	return (pal);
+}
+
+
+
+/**
+ * is_palindrome - Check if linked list is palindrome.
+ * @head: give list
+ * @right: Last element take it in recursion.
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head;
-	int size = sizeof(int);
-	int i = 1;
-	int left = 0;
-	int right = i - 1;
-	int *arr = malloc(100000 * sizeof(int));
+	int res;
 
-	arr[0] = (*head)->n;
-
-	if (*head == NULL)
-		return (1);
-
-	temp = temp->next;
-
-	while (temp != NULL)
-	{
-		size += sizeof(int);
-		arr = realloc(arr, size);
-		arr[i] = temp->n;
-		i++;
-		temp = temp->next;
-	}
-	for (; left < right; left++, right--)
-	{
-		if (arr[left] != arr[right])
-			return (0);
-	}
-	return (1);
+	if (!head)
+		return (0);
+	res = PalindromeRecur(head, *head);
+	return (res);
 }
