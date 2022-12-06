44b6636cb9bb5faa52cbc9f7a34cd3db19b10f3c
phemsie <femi_oworu@yahoo.com>
2022-12-06 08:45:22 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import calculator_1 as calc
     import sys
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
+    ac = len(sys.argv) - 1
+    if ac != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-        op = sys.argv[2]
-        if op != '+' and op != '-' and op != '*' and op != '/':
+        operator = sys.argv[2]
+        if operator not in ('+', '-', '*', '/'):
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
-            from calculator_1 import add, sub, mul, div
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
 
-            if op == '+':
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            elif op == '-':
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif op == '*':
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            else:
-                print("{} / {} = {}".format(a, b, div(a, b)))
+            a = float(sys.argv[1]) if '.' in sys.argv[1] else int(sys.argv[1])
+            b = float(sys.argv[3]) if '.' in sys.argv[3] else int(sys.argv[3])
+
+            if operator == '+':
+                print(f"{a} {operator} {b} = {calc.add(a, b)}")
+            elif operator == '-':
+                print(f"{a} {operator} {b} = {calc.sub(a, b)}")
+            elif operator == '*':
+                print(f"{a} {operator} {b} = {calc.mul(a, b)}")
+            elif operator == '/':
+                print(f"{a} {operator} {b} = {calc.div(a, b)}")
