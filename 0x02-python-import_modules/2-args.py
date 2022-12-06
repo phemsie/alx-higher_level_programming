b4a42d272cc98a795c0e1b31ce3cba727644c97f
phemsie <femi_oworu@yahoo.com>
2022-12-06 08:34:35 -0800

    python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     ac = len(sys.argv) - 1
     print("{:d} {:s}{:s}".format(ac, 'argument' if ac == 1 else 'arguments',
-                                                                    '.' if ac == 0 else ':'))
+        '.' if ac == 0 else ':'))
     for i in range(1, ac + 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
