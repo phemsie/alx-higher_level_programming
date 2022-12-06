6fe7c639362437056e68ac49e8c24059c8d3c39a
phemsie <femi_oworu@yahoo.com>
2022-12-06 08:32:06 -0800

    python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
-
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    ac = len(sys.argv) - 1
+    print("{:d} {:s}{:s}".format(ac, 'argument' if ac == 1 else 'arguments',
+                                                                    '.' if ac == 0 else ':'))
+    for i in range(1, ac + 1):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
