bf14faceb8ad4d102875375085624acdf354ee30
phemsie <femi_oworu@yahoo.com>
2022-12-07 17:15:52 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for k, value in list(a_dictionary.items()):
-        if k == key:
-            del a_dictionary[key]
-            return a_dictionary
+    a_dictionary.pop(key, None)
+    return a_dictionary
