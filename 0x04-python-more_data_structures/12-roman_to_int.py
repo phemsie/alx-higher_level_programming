ab7d791eb9c227804090bef3450a8b2d8e1f1bc6
phemsie <femi_oworu@yahoo.com>
2022-12-08 09:54:09 -0800

    more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,10 @@
 #!/usr/bin/python3
-
-def get_value(str):
-    rom_table = [
-            {"M": 1000, "m": 1000, "CM": 900, "cm": 900},
-            {"D": 500, "d": 500, "CD": 400, "cd": 400},
-            {"C": 100, "c": 100, "XC": 90, "xc": 90},
-            {"L": 50, "l": 50, "XL": 40, "xl": 40},
-            {"X": 10, "x": 10, "IX": 9, "ix": 9},
-            {"V": 5, "v": 5, "IV": 4, "iv": 4},
-            {"I": 1, "i": 1}
-            ]
-    for row in rom_table:
-        if str in row:
-            return row[str]
-        return -1
-
-    def roman_to_int(roman_string):
-        if roman_string is None:
-            return 0
-        if not roman_string.isalpha():
-            return 0
-
-        stat = None
-        result = 0
-
-        i = 0
-        while i < (len(roman_string)):
-            if (stat := get_value(roman_string[i:i+2])) != -1:
-                result = result + stat
-                i = i + 2
-                continue
-            if (stat := get_value(roman_string[i])) != -1:
-                result = result + stat
-                i = i + 1
-                continue
-            else:
-                return 0
-            return result
+def roman_to_int(roman_string):
+    table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    prev, sum = 0, 0
+    for c in roman_string:
+        sum += table[c] if table[c] <= prev else table[c] - prev * 2
+        prev = table[c]
+        return sum
