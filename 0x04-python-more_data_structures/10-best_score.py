463351c0c8449b93088409a3065772c60f96e302
phemsie <femi_oworu@yahoo.com>
2022-12-07 17:18:41 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    """ returns a key with the biggest integer value """
-    maxm = 0
-    person = None
-    if a_dictionary:
-        for key in a_dictionary:
-            if a_dictionary[key] > maxm:
-                maxm = a_dictionary[key]
-                person = key
-                return person
+    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
