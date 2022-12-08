47ee324ab7fadd2585663c4d18fb179381bf0ba6
phemsie <femi_oworu@yahoo.com>
2022-12-07 18:31:19 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary.keys()):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
+    if value not in a_dictionary.values():
+        return a_dictionary
+
+    new = a_dictionary.copy()
+    for k, v in new.items():
+        if v == value:
+            del a_dictionary[k]
+
             return a_dictionary
