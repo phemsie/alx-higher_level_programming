1328ee5693d389e1abcc45a6787e4d6bd79b501c
phemsie <femi_oworu@yahoo.com>
2022-12-07 18:01:32 -0800

    python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [1 if l != search else replace for l in my_list]
+    return [l if l != search else replace for l in my_list]
