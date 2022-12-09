00d21c1d8f18655d63ecd04fc3a20b4b145b79b7
phemsie <femi_oworu@yahoo.com>
2022-12-09 08:49:34 -0800

    more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-    prev, sum = 0, 0
-    for c in roman_string:
-        sum += table[c] if table[c] <= prev else table[c] - prev * 2
-        prev = table[c]
-        return sum
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+            return (max_list - to_sub)
+
+        def roman_to_int(roman_string):
+            if not roman_string:
+                return 0
+
+            if not isinstance(roman_string, str):
+                return 0
+
+            rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+            list_keys = list(rom_n.keys())
+            num = 0
+            last_rom = 0
+            list_num = [0]
+            for ch in roman_string:
+                for r_num in list_keys:
+                    if r_num == ch:
+                        if rom_n.get(ch) <= last_row:
+                            num += to_subtract(list_num)
+                            list_num = [rom_n.get(ch)]
+                        else:
+                            list_num.append(rom_n.get(ch))
+                            last_rom = rom_n.get(ch)
+                            num += to_subtract(list_num)
+
+                            return (num)
