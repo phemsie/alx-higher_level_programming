a8a6fb03f97f0ff0bfa1e5e1a9a467aae50ce175
phemsie <femi_oworu@yahoo.com>
2022-12-19 23:14:59 +0100

    python-exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,11 +4,11 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if i > a:
-                raise Exception('Too far')
+            if (i > a):
+                raise Exception("Too far")
             else:
-                result += a ** b / i
+                result += (a ** b) / i
         except:
             result = b + a
             break
-    return
+    return (result)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    nb_print = 0
-    for i in range(nb_print, x):
+    ret = 0
+    for i in range(0, x):
         try:
-                print("{:d}".format(my_list[i]), end="")
-                nb_print += 1
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
         except (ValueError, TypeError):
-            pass
-    print()
-    return nb_print
+            continue
+    print("")
+    return
