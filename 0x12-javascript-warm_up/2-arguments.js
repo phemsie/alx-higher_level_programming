cb9b380229a0540600f6e8f7871e5f208f286006
phemsie <femi_oworu@yahoo.com>
2023-03-13 14:43:04 +0100

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 0) {
+  console.log('No argument');
+} else if (process.argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
