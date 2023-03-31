fab87b6a40e71ca8abb79f37c8a8592b683ac41e
phemsie <femi_oworu@yahoo.com>
2023-03-31 12:40:28 +0100

    python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://intranet.hbtn.io/status """
+"""fetches https://intranet.hbtn.io/status using the urllib package
+"""
 import urllib.request
-
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        charset = html.decode('utf-8')
-        print("Body response:")
-        print(
-            "\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-            .format(type(html), html, charset))
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    data = response.read()
+    print("Body response:")
+    print("	- type:", type(data))
+    print("	- content:", data)
+    print("	- utf8 content:", data.decode('utf-8'))
