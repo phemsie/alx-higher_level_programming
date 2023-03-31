1cd220ad220be780d3c0d18e4c099de54ccc83d4
phemsie <femi_oworu@yahoo.com>
2023-03-31 12:45:36 +0100

    python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://intranet.hbtn.io/status """
-import requests
-
-
+"""fetches https://intranet.hbtn.io/status
+using requests
+"""
 if __name__ == "__main__":
-    html = requests.get("https://intranet.hbtn.io/status")
+    import requests
+    req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print(
-        "\t- type: {}\n\t- content: {}"
-        .format(type(html.text), html.text))
-
+    print("	- type:", type(req.text))
+    print("	- content:", req.text)
